02a6455d55b8ee43c48f3a9cca4c57e37cf54a04
niranjan-Infofaces <36027271+niranjan-Infofaces@users.noreply.github.com>
2018-02-06 14:51:04 +0530

    Update hotel.js

--- hotel.js
+++ hotel.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+require("http");
+const DialogflowApp = require('actions-on-google').DialogflowApp;
 
 const restService = express();
 
@@ -10,9 +12,54 @@ restService.use(
     extended: true
   })
 );
+function sillyNameMaker(req, res) {
+const app = new DialogflowApp({request: req, response: res});
+
+const WELCOME_INTENT = 'input.welcome';  // the action name from the Dialogflow intent
+const NUMBER_INTENT = 'input.number';  // the action name from the Dialogflow intent
+const NUMBER_ARGUMENT = 'input.mynum'; // the action name from the Dialogflow intent
+console.log(req)
+function welcomeIntent (app) {
+  app.ask('Welcome to number echo! Say a number.');
+}
+
+function numberIntent (app) {
+  let number = app.getArgument(NUMBER_ARGUMENT);
+  app.tell('You said ' + number);
+}
+let actionMap = new Map();
+  actionMap.set(WELCOME_INTENT, welcomeIntent);
+  actionMap.set(NUMBER_INTENT, numberIntent);
+  app.handleRequest(actionMap);
+  
+  function responseHandler (assistant) {
+    console.log("okok")
+    // intent contains the name of the intent you defined in the Actions area of API.AI
+    let intent = assistant.getIntent();
+    switch (intent) {
+      case WELCOME_INTENT:
+        assistant.ask('Welcome! Say a number.');
+        break;
+
+      case NUMBER_INTENT:
+        let number = assistant.getArgument(NUMBER_ARGUMENT);
+        assistant.tell('You said ' + number);
+        break;
+    }
+  }
+  // you can add the function name instead of an action map
+  app.handleRequest(responseHandler);
+ 
+
+}
+
+
 
 restService.use(bodyParser.json());
-
+restService.post('/', function (req, res) {
+ // console.log(req.body);
+  sillyNameMaker(req, res);
+})
 restService.post("/echo", function(req, res) {
   //  var menu = "idli"   
    var menu = "idli, vada, dosa"

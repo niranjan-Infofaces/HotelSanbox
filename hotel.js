9b12dd3da883b27800c78dc4a52397be38f061d1
niranjan-Infofaces <niranjand@infofaces.co.in>
2018-02-06 15:00:21 +0530

    hotel 6Feb

--- hotel.js
+++ hotel.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 
 const restService = express();
